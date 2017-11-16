8b7499df6112b886e4ed919cec642c3e1bf231d0
Nowosad <tupiszakaczki@gmail.com>
2017-11-16 14:35:57 -0500

    wq_algorithms docs

---
+++ R/wq_algorithms.R
@@ -0,0 +1,9 @@
+#' wq_algorithms database
+#'
+#' A dataset containing the information about the water quality algorithms
+#'
+#' @format A tibble with 23 rows and 3 variables:
+#' * name: algorithm name
+#' * funs: algorithm function
+#' * sentinel2: list of the bands used from the Sentinel 2 satellite
+"wq_algorithms"
