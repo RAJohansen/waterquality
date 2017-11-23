98959824e23869241caf906ceb838b728938b4e5
jn <tupiszakaczki@gmail.com>
2017-11-23 17:44:30 -0500

    updated main dataset

--- R/wq_algorithms.R
+++ R/wq_algorithms.R
@@ -2,8 +2,12 @@
 #'
 #' A dataset containing the information about the water quality algorithms
 #'
-#' @format A tibble with 23 rows and 3 variables:
+#' @format A tibble with 24 rows and 7 variables:
 #' * name: algorithm name
 #' * funs: algorithm function
+#' * worldview2: list of the bands used from the WorldView-2 satellite
 #' * sentinel2: list of the bands used from the Sentinel 2 satellite
+#' * landsat8: list of the bands used from the Landsat 8 satellite
+#' * modis: list of the bands used from the MODIS instrument
+#' * meris: list of the bands used from the Sentinel 2 instruments
 "wq_algorithms"
