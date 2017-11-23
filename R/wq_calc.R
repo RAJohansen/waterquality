6dacb2662d040b466cf4f41b81c51cee98a787d5
jn <tupiszakaczki@gmail.com>
2017-11-23 18:35:17 -0500

    nicer function

--- R/wq_calc.R
+++ R/wq_calc.R
@@ -4,7 +4,7 @@
 #'
 #' @param raster_stack RasterStack containing a satellite data
 #' @param alg Name of the algorithm or "all"
-#' @param sat Name of the satellite
+#' @param sat Name of the satellite or instrument ("worldview2", "sentinel2", "landsat8", "modis", or "meris")
 #' 
 #' @importFrom raster overlay stack
 #'
@@ -14,15 +14,28 @@
 #' \dontrun{(
 #' library(raster)
 #' s2 = stack(system.file("raster/S2_Taylorsville.tif", package = "algae"))
-#' s2_wq = wq_calc(s2)
+#' s2_wq = wq_calc(s2, alg = "all", sat = "sentinel2")
+#' l8 = stack(system.file("raster/L8_Taylorsville.tif", package = "algae"))
+#' l8_wq = wq_calc(s2, alg = "all", sat = "landsat8")
+#' 
+#' # errors
+#' s2_wq1 = wq_calc(s2[[1]], alg = "all", sat = "sentinel2")
+#' s2_wq2 = wq_calc(s2, alg = "all", sat = "my_satellite")
+#' s2b = s2[[1:4]]
+#' s2_wq3 = wq_calc(s2b, alg = "all", sat = "my_satellite")
 #' )}
 #' @export
 wq_calc = function(raster_stack, alg = "all", sat = "sentinel2"){
+  if (!is(raster_stack, 'RasterStack')) stop ("Input object needs to be of the RasterStack class")
+  algorithms_sel = algae::wq_algorithms[algae::wq_algorithms$satellite == sat, ]
+  if (nrow(algorithms_sel) == 0) stop ('Unknown satellite or instrument. Please select one of: "worldview2", "sentinel2", "landsat8", "modis", or "meris"')
+  # check bands
   result = list()
-  for (i in seq_len(nrow(algae::wq_algorithms))){
-    raster_stack_sel = raster_stack[[algae::wq_algorithms$sentinel2[[i]]]]
-    result[[i]] = overlay(raster_stack_sel, fun = algae::wq_algorithms$funs[[i]])
-    names(result[[i]]) = algae::wq_algorithms$name[[i]]
+  for (i in seq_len(nrow(algorithms_sel))){
+    print(algorithms_sel$name[[i]])
+    raster_stack_sel = raster_stack[[algorithms_sel$bands[[i]]]]
+    result[[i]] = overlay(raster_stack_sel, fun = algorithms_sel$funs[[i]])
+    names(result[[i]]) = algorithms_sel$name[[i]]
   }
   stack(result)
 }
