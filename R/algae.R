73bb11303e19e4383b26b5e30b6f217f6f491a7e
jn <tupiszakaczki@gmail.com>
2017-11-23 17:56:17 -0500

    package docs template

---
+++ R/algae.R
@@ -0,0 +1,11 @@
+#' algae: A package for ....
+#'
+#' The foo package provides three categories of important functions:
+#' AA, BB, CC
+#' 
+#' @section AA functions:
+#' The OO functions ...
+#'
+#' @docType package
+#' @name foo
+NULL
