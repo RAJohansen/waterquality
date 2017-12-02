a9aeae041e8618591bfb5d06347fa306e280b92a
jn <tupiszakaczki@gmail.com>
2017-12-02 13:31:28 -0500

    type added

--- R/wq_calc.R
+++ R/wq_calc.R
@@ -3,7 +3,8 @@
 #' Calculates a set of water quality indices
 #'
 #' @param raster_stack RasterStack containing a satellite data
-#' @param alg Name of the algorithm or "all"
+#' @param alg Name (e.g. [Am09KBBI()]) or type of the algorithm ("chlorophyll", "phychocyanin", "turbidity") or "all"
+#' @param type Type of the algorithm 
 #' @param sat Name of the satellite or instrument ("worldview2", "sentinel2", "landsat8", "modis", or "meris")
 #' @param ... Other arguments passed on to [stack()]
 #' 
@@ -18,19 +19,21 @@
 #' s2 = stack(system.file("raster/S2_Taylorsville.tif", package = "waterquality"))
 #' s2_wq = wq_calc(s2, alg = "all", sat = "sentinel2")
 #' s2_Al10SABI = wq_calc(s2, alg = "Al10SABI", sat = "sentinel2")
-#' s2_two_alg = wq_calc(s2, alg = c("Al10SABI", "Am092Bsub"), sat = "sentinel2")
+#' s2_two_alg = wq_calc(s2, alg = c("TurbChip09NIROverGreen", "Am092Bsub"), sat = "sentinel2")
 #' 
 #' l8 = stack(system.file("raster/L8_Taylorsville.tif", package = "waterquality"))
 #' l8_wq = wq_calc(s2, alg = "all", sat = "landsat8")
 #' )}
 #' @export
-wq_calc = function(raster_stack, alg = "all", sat = "sentinel2", ...){
+wq_calc = function(raster_stack, alg = "all", type = "all", sat, ...){
   if (!is(raster_stack, 'RasterStack')) stop ("Input object needs to be of the RasterStack class")
   if ("all" %in% alg){
     algorithms_sel = waterquality::wq_algorithms
+  } else if (any(c("chlorophyll", "phychocyanin", "turbidity") %in% alg)){
+    algorithms_sel = waterquality::wq_algorithms[waterquality::wq_algorithms$type %in% alg, ]
   } else{
     algorithms_sel = waterquality::wq_algorithms[waterquality::wq_algorithms$name %in% alg, ]
-    if (nrow(algorithms_sel) == 0) stop ("Unknown algorithm name/names: ",
+    if (nrow(algorithms_sel) == 0) stop ("Unknown algorithm name/type: ",
                                          paste(alg, collapse = ", "))
     alg_valid = alg %in% algorithms_sel$name
     if (!all(alg_valid)) warning("Some of the algorithms do not exist: ", 
@@ -52,4 +55,3 @@ wq_calc = function(raster_stack, alg = "all", sat = "sentinel2", ...){
   }
   stack(result, ...)
 }
-
--- tests/testthat/test-calc.R
+++ tests/testthat/test-calc.R
@@ -2,6 +2,10 @@ context("wq_calc")
 test_that("wq_calc works correctly", {
   s2 = stack(system.file("raster/S2_Taylorsville.tif", package = "waterquality"))
   
+  # test number of output layers
+  expect_equal(nlayers(wq_calc(s2, alg = c("Am092Bsub"), sat = "sentinel2")), 1)
+  expect_equal(nlayers(wq_calc(s2, alg = c("TurbChip09NIROverGreen", "Am092Bsub"), sat = "sentinel2")), 2)
+
   # warnings
   expect_warning(wq_calc(s2, alg = c("Al10SABI", "Am092Bsub", "my_alg"), sat = "sentinel2"))
   expect_warning(wq_calc(s2, alg = c("my_alg1", "Al10SABI", "Am092Bsub", "my_alg"), sat = "sentinel2"))
