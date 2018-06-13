9aa0045c87e8d56706970a6cf00607d0c69ff1bf
Richard Johansen <richard.johansen@uc.edu>
2018-06-13 15:49:18 -0400

    Takes ~4-5 minutes to run full scene then crop

--- data-raw/Data_Preprocessing.R
+++ data-raw/Data_Preprocessing.R
@@ -9,32 +9,29 @@ Image_Dir <- "C:/temp/S2A_MSIL1C_20180609T161901_N0206_R040_T16SGJ_20180609T1943
 all_rasters_names = dir(Image_Dir, pattern = "*.jp2$", full.names = TRUE)
 
 ### Create Raster Output Template-----------------------------------------------
-
-#Import Shapefile of Area of Interest to crop images (Shapefile)
-AOI <- shapefile("C:/temp/S2/eastfork_lake")
-#Reproject shapefile if needed Harsha Lake Projection (UTM)
-Projection <- "+proj=utm +zone=16 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
-AOI <- spTransform(AOI, CRS(Projection))
-
-#Use Raster with appropiate resolution for template 
 #For Sentinel-2 we want our final output to be in 20m so we can us Band 5
-raster5 = raster(all_rasters_names[[5]])
-raster_template = raster(raster5)
-#template = raster(all_rasters_names[[5]])
-#template_crop <- crop(template, AOI)
-#raster_template = raster(template_crop)
+raster_B5 = raster(all_rasters_names[[5]])
+raster_template = raster(raster_B5)
 
 ### Resample and Stack All Rasters ---------------------------------------------
-output_stack = all_rasters_names %>% 
+resampled_stack = all_rasters_names %>% 
   lapply(raster) %>% 
   lapply(resample, raster_template) %>%
   stack()
 
-Harsha <- crop(output_stack, AOI)
+
+#Import Shapefile of Area of Interest to crop images (Shapefile)
+AOI <- shapefile("C:/temp/S2/eastfork_lake")
+#Reproject shapefile if needed Harsha Lake Projection (UTM Zone 16)
+Projection <- "+proj=utm +zone=16 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+AOI <- spTransform(AOI, CRS(Projection))
+
+#Crop Resampled Raster
+Harsha <- crop(resampled_stack, AOI)
 
 #Write Final Stacked S2 Image
 writeRaster(x = Harsha,
-            filename="C:/temp/S2/S2_06092018_Harsha.tif",
+            filename="C:/temp/S2_06092018_Harsha.tif",
             format = "GTiff", # save as a tif
             datatype='FLT4S', # save as a float
             overwrite = F) #Overwrites same named file
