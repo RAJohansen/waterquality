d5210973942ec0e9dd06e24a2d0d1d562a8919bf
Richard Johansen <richard.johansen@uc.edu>
2018-06-14 09:04:26 -0400

    Added crop after resample, so final output is only the cropped stack (same run time but eliminates saving the large intermediate data)
    @Nowosad

--- data-raw/Data_Preprocessing.R
+++ data-raw/Data_Preprocessing.R
@@ -3,36 +3,34 @@ library(gdalUtils)
 library(raster)
 library(magrittr)
 
-#Define path to folder location of jpg2000 imagery
+###Define Inputs----------------------------------------------------------------
+#Image Directory Folder
 Image_Directory <- "C:/temp/S2A_MSIL1C_20180609T161901_N0206_R040_T16SGJ_20180609T194342.SAFE/GRANULE/L1C_T16SGJ_A015481_20180609T162519/IMG_DATA"
 
-### Extacts all files from Image Directory with extention .jp2------------------
-Rasters = dir(Image_Directory, pattern = "*.jp2$", full.names = TRUE)
-
-### Create Raster Template------------------------------------------------------
-#For Sentinel-2 we want our final output to be in 20m so we can us Band 5
-raster_B5 = raster(Rasters[[5]])
-raster_template = raster(raster_B5)
-
-### Resample and Stack All Rasters ---------------------------------------------
-resampled_stack = Rasters %>% 
-  lapply(raster) %>% 
-  lapply(resample, raster_template) %>%
-  stack()
-
-### Crop Full Scene using Shapefile---------------------------------------------
-#Import Shapefile of Area of Interest
+#Import Shapefile of Area of Interest 
 AOI <- shapefile("C:/temp/S2/eastfork_lake")
 
-#Reproject shapefile if needed (Harsha Lake uses UTM Zone 16)
+#Reproject AOI if needed (Example - UTM Zone 16)
 Projection <- "+proj=utm +zone=16 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 AOI <- spTransform(AOI, CRS(Projection))
 
-#Use AOI to Crop Resampled Raster Stack
-Harsha_Lake <- crop(resampled_stack, AOI)
+### Extacts all raster files from Image Directory with extention .jp2-----------
+Rasters = dir(Image_Directory, pattern = "*.jp2$", full.names = TRUE)
 
-#Write Final Stacked S2 Image
-writeRaster(x = Harsha_Lake,
+### Create Template for Final Raster Stack--------------------------------------
+#For Sentinel-2 we want our final output to be in 20m so use Band 5
+raster_B5 = raster(Rasters[[5]])
+raster_template = raster(raster_B5)
+
+### Resample, Crop, & Stack All Images -----------------------------------------
+raster_stack = Rasters %>% 
+  lapply(raster) %>% 
+  lapply(resample, raster_template) %>%
+  lapply(crop, AOI) %>%
+  stack()
+
+#Save Final Stacked Image as Tiff
+writeRaster(x = raster_stack,
             filename="C:/temp/Harsha_Lake.tif",
             format = "GTiff", # save as a tif
             datatype='FLT4S', # save as a float
