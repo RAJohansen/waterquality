a46bcf2a8cdb20bc9c8bb808f99944224aee35da
jn <tupiszakaczki@gmail.com>
2018-06-20 20:34:07 -0400

    test for algorithms type

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,4 +2,5 @@ context("wq_algortihms")
 test_that("wq_algortihms are correct", {
   # data is intact
   expect_equal(dim(wq_algorithms), c(136, 5))
+  expect_equal(unique(wq_algorithms$type), c("chlorophyll", "phycocyanin", "turbidity"))
 })
