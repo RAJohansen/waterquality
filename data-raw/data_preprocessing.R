2a77aef0199a6f284fe1d19d634ed33616bb6d28
Richard Johansen <richard.johansen@uc.edu>
2018-07-19 09:13:59 -0400

    Updated Vignette and data_preprocessing file to match

--- data-raw/data_preprocessing.R
+++ data-raw/data_preprocessing.R
@@ -7,6 +7,8 @@ library(magrittr)
 #Image Directory Folder
 Image_Directory = "C:/temp/S2A_MSIL1C_20180609T161901_N0206_R040_T16SGJ_20180609T194342.SAFE/GRANULE/L1C_T16SGJ_A015481_20180609T162519/IMG_DATA"
 
+### Extacts all raster files from Image Directory with extention .jp2
+Rasters = dir(Image_Directory, pattern = "*.jp2$", full.names = TRUE)
 #Import Shapefile of Area of Interest 
 AOI = st_read("C:/temp/S2/eastfork_lake.shp")
 
@@ -14,9 +16,6 @@ AOI = st_read("C:/temp/S2/eastfork_lake.shp")
 Projection = "+proj=utm +zone=16 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 AOI = st_transform(AOI, Projection)
 AOI = as(AOI, "Spatial")
-
-### Extacts all raster files from Image Directory with extention .jp2-----------
-Rasters = dir(Image_Directory, pattern = "*.jp2$", full.names = TRUE)
 
 ### Create Template for Final Raster Stack--------------------------------------
 #For Sentinel-2 we want our final output to be in 20m so use Band 5
@@ -30,18 +29,15 @@ raster_stack = Rasters %>%
   lapply(crop, AOI) %>%
   stack()
 
-### Mask cropped image 
+### Mask cropped image---------------------------------------------------------- 
 #Further reducing size of stacked image
 raster_mask <- mask(raster_stack,AOI)
 
-### Band Subset
-#Selects only Bands 2-8A
-S2_Harsha <- raster_mask[2:9]
-S2_Harsha <- x[[2:9]]
-x <- stack("C:/R_Packages/waterquality/inst/raster/S2_Harsha.tif")
+### Band Subset-----------------------------------------------------------------
+S2_Harsha <- raster_mask[1:9] #Sentinel-2 Algorithms Only Use Bands 1-8A
 
 ### Save Final Stacked Image as Tiff -------------------------------------------
 writeRaster(x = S2_Harsha,
             filename= "C:/temp/S2_Harsha.tif", # save as a tif
-            datatype ="FLT4S", # save as a float
+            datatype ="FLT4S", # save as a float 4 significant digits
             overwrite = FALSE) #Overwrites same named file
