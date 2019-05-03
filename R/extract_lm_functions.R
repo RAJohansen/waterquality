7ce7fefbb2843f99c2276cde20eb9cd020368e4b
Richard Johansen <richard.johansen@uc.edu>
2019-05-03 08:59:29 -0400

    Added extract_lm_functions
    extract_lm
    extract_lm_cv
    extract_lm_cv_multi
    extract_lm_cv_all

---
+++ R/extract_lm_functions.R
@@ -0,0 +1,133 @@
+#' Run linear model (lm)
+#'
+#'The function runs a linear model on a single x and y variable
+#' and returns a data frame containing the following:
+#' r^2, p-value, slope, and intercept of the model
+#'
+#' @param y independent variable
+#' @param x dependent variable
+#' @param df data frame object
+#' @return A dataframe of the model results
+#'
+#' @references Johansen, Richard; et al. (2018). Evaluating the portability of satellite derived chlorophyll-a algorithms for temperate inland lakes using airborne hyperspectral imagery and dense surface observations. Harmful Algae. 76. 10.1016/j.hal.2018.05.001.
+#' @references  R Core Team (2018). R: A language and environment for statistical computing. R Foundation for Statistical Computing, Vienna, Austria. URL https://www.R-project.org/.
+#'
+#' @family lm models
+#' @export
+extract_lm <- function(y, x, df){
+  my_lm = lm(y ~ x)
+  R_Squared = summary(my_lm)$r.squared
+  P_Value = summary(my_lm)$coefficients[8]
+  Slope = summary(my_lm)$coefficients[2]
+  Intercept = summary(my_lm)$coefficients[1]
+  data_frame(R_Squared = R_Squared, Slope = Slope, Intercept = Intercept, P_Value = P_Value)
+}
+
+#' Run linear model with crossvalidation
+#'
+#'The function runs a linear model on a single x and y variable and conducts
+#' a k-folds cross validation, which returns a data frame containing the following:
+#' The r^2, p-value, slope, intercept of the global lm model &
+#' average r^2, average RMSE, average MAE from the crossvalidated model
+#'
+#' @param y independent variable
+#' @param x dependent variable
+#' @param df data frame object
+#' @param folds the number of folds to be used in the cross validation model
+#' @param nrepeats the number of iterations to be used in the cross validation model
+#'
+#' @return A dataframe of the model results
+#'
+#' @references Johansen, Richard; et al. (2018). Evaluating the portability of satellite derived chlorophyll-a algorithms for temperate inland lakes using airborne hyperspectral imagery and dense surface observations. Harmful Algae. 76. 10.1016/j.hal.2018.05.001.
+#' @references  R Core Team (2018). R: A language and environment for statistical computing. R Foundation for Statistical Computing, Vienna, Austria. URL https://www.R-project.org/.
+#' @references   Max Kuhn. Contributions from Jed Wing, Steve Weston, Andre Williams, Chris Keefer, Allan Engelhardt, Tony Cooper, Zachary Mayer, Brenton Kenkel, the R Core Team, Michael Benesty, Reynald Lescarbeau, Andrew Ziem, Luca Scrucca, Yuan Tang, Can Candan and Tyler Hunt. (2018). caret: Classification and Regression Training. R package version 6.0-81. https://CRAN.R-project.org/package=caret
+#'
+#' @family lm models
+#' @export
+extract_lm_cv <- function(y, x, df, folds = 3, nrepeats =5){
+  my_formula = as.formula(paste(y, "~" ,x))
+  caret_model = caret::train(form = my_formula,
+                             data = df,
+                             method = "lm",
+                             na.action = na.exclude,
+                             #repeated k-fold validation
+                             trControl = caret::trainControl(method = "repeatedcv",
+                                                             number = folds, repeats = nrepeats))
+  my_lm = caret_model$finalModel
+  CV_R_Squared = getTrainPerf(caret_model)[, "TrainRsquared"]
+  RMSE = getTrainPerf(caret_model)[, "TrainRMSE"]
+  MAE = getTrainPerf(caret_model)[, "TrainMAE"]
+  R_Squared = summary(my_lm)$r.squared
+  P_Value = summary(my_lm)$coefficients[8]
+  Slope = summary(my_lm)$coefficients[2]
+  Intercept = summary(my_lm)$coefficients[1]
+  data_frame(R_Squared = R_Squared, Slope = Slope, Intercept = Intercept, P_Value = P_Value,
+             CV_R_Squared = CV_R_Squared, RMSE = RMSE, MAE = MAE)
+}
+
+#' Run linear model with crossvalidation over multiple x and y variables
+#'
+#'The function runs a linear model on a list of x and list of y variables and conducts
+#' a k-folds cross validation, which returns a data frame containing the following:
+#' The r^2, p-value, slope, intercept of the global lm model &
+#' average r^2, average RMSE, average MAE from the crossvalidated model
+#'
+#' @param parameters the list of independent variables to be evaluated
+#' @param Algorithms the list of dependent variables to be evaluated
+#' @param df data frame object
+#' @param folds the number of folds to be used in the cross validation model
+#' @param nrepeats the number of iterations to be used in the cross validation model
+#'
+#' @return A dataframe of the model results
+#'
+#' @references Johansen, Richard; et al. (2018). Evaluating the portability of satellite derived chlorophyll-a algorithms for temperate inland lakes using airborne hyperspectral imagery and dense surface observations. Harmful Algae. 76. 10.1016/j.hal.2018.05.001.
+#' @references  R Core Team (2018). R: A language and environment for statistical computing. R Foundation for Statistical Computing, Vienna, Austria. URL https://www.R-project.org/.
+#' @references   Max Kuhn. Contributions from Jed Wing, Steve Weston, Andre Williams, Chris Keefer, Allan Engelhardt, Tony Cooper, Zachary Mayer, Brenton Kenkel, the R Core Team, Michael Benesty, Reynald Lescarbeau, Andrew Ziem, Luca Scrucca, Yuan Tang, Can Candan and Tyler Hunt. (2018). caret: Classification and Regression Training. R package version 6.0-81. https://CRAN.R-project.org/package=caret
+#'
+#' @family lm models
+#' @export
+extract_lm_cv_multi <- function(parameters, Algorithms, df,folds = 3, nrepeats = 5){
+  list = list()
+  for (i in seq_along(parameters)) {
+    names(Algorithms) <- Algorithms %>% purrr::map_chr(., ~ paste0(parameters[[i]], "_",.))
+    list[[i]] = Algorithms %>%
+      map_dfr(~extract_lm_caret(y = parameters[[i]], x = ., df = df, folds = folds, nrepeats = nrepeats), .id="Algorithms")
+  }
+  results <- (do.call(rbind, list))
+  assign(paste0(format(Sys.time(), "Results_%Y-%m-%d_%H%M")),results, envir = .GlobalEnv)
+}
+
+
+#' Run linear model with crossvalidation over multiple y and all numeric dependent variables in a data frame
+#'
+#'The function runs a linear model on a list of x and list of y variables and conducts
+#' a k-folds cross validation, which returns a data frame containing the following:
+#' The r^2, p-value, slope, intercept of the global lm model &
+#' average r^2, average RMSE, average MAE from the crossvalidated model
+#'
+#' @param parameters the list of independent variables to be evaluated
+#' @param df data frame object
+#' @param folds the number of folds to be used in the cross validation model
+#' @param nrepeats the number of iterations to be used in the cross validation model
+#'
+#' @return A dataframe of the model results
+#'
+#' @references Johansen, Richard; et al. (2018). Evaluating the portability of satellite derived chlorophyll-a algorithms for temperate inland lakes using airborne hyperspectral imagery and dense surface observations. Harmful Algae. 76. 10.1016/j.hal.2018.05.001.
+#' @references  R Core Team (2018). R: A language and environment for statistical computing. R Foundation for Statistical Computing, Vienna, Austria. URL https://www.R-project.org/.
+#' @references   Max Kuhn. Contributions from Jed Wing, Steve Weston, Andre Williams, Chris Keefer, Allan Engelhardt, Tony Cooper, Zachary Mayer, Brenton Kenkel, the R Core Team, Michael Benesty, Reynald Lescarbeau, Andrew Ziem, Luca Scrucca, Yuan Tang, Can Candan and Tyler Hunt. (2018). caret: Classification and Regression Training. R package version 6.0-81. https://CRAN.R-project.org/package=caret
+#'
+#' @family lm models
+#' @export
+extract_lm_cv_all <- function(parameters, df, folds = 3, nrepeats = 5){
+  list = list()
+  for (i in seq_along(parameters)) {
+    Algorithms = df %>%
+      select(which(sapply(.,class)=="numeric"),-parameters) %>%
+      names()
+    names(Algorithms) <- Algorithms %>% purrr::map_chr(., ~ paste0(parameters[[i]], "_",.))
+    list[[i]] = Algorithms %>%
+      map_dfr(~extract_lm_caret(y = parameters[[i]], x = ., df = df, folds = folds, nrepeats = nrepeats), .id="Algorithms")
+  }
+  results <- (do.call(rbind, list))
+  assign(paste0(format(Sys.time(), "Results_%Y-%m-%d_%H%M")),results, envir = .GlobalEnv)
+}
