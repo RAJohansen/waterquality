e5b761a1fc7afbafe3729d545a647e7bdd89756b
Richard <rjohansen73@gmail.com>
2021-03-20 12:15:53 -0500

    Corrected error in mapping. So this can be used with or without sampling locations.

--- R/map_wq.R
+++ R/map_wq.R
@@ -18,20 +18,30 @@ Map_WQ_raster <- function(WQ_raster, sample_points, map_title, raster_style = "q
     stop("package tmap required, please install it first") 
   tmap::tm_shape(WQ_raster) +
     tmap::tm_raster(title = map_title,
-              style = raster_style, n = 8, midpoint = NA,
-              palette = "viridis",
-              legend.reverse = TRUE,
-              legend.hist = histogram) +
+                    style = raster_style, n = 8, midpoint = NA,
+                    palette = "viridis",
+                    legend.reverse = TRUE,
+                    legend.hist = histogram) +
     tmap::tm_legend(outside = TRUE, hist.width = 2) +
-    tmap::tm_shape(sample_points) + 
-    tmap::tm_dots(size = 0.1, alpha = 0.75) +
-    tmap::tm_scale_bar(text.size = 0.75) + 
-    tmap::tm_grid(labels.inside.frame = FALSE,
-            labels.size = 1,
-            n.x = 5,
-            n.y = 5,
-            projection = "+proj=longlat",
-            alpha = 0.35)
+    if(missing(sample_points)){
+      tmap::tm_scale_bar(text.size = 0.75) + 
+        tmap::tm_grid(labels.inside.frame = FALSE,
+                      labels.size = 1,
+                      n.x = 5,
+                      n.y = 5,
+                      projection = "+proj=longlat",
+                      alpha = 0.35)
+    }else{
+      tmap::tm_shape(sample_points) +
+        tmap::tm_dots(size = 0.1, alpha = 0.75)
+      tmap::tm_scale_bar(text.size = 0.75) + 
+        tmap::tm_grid(labels.inside.frame = FALSE,
+                      labels.size = 1,
+                      n.x = 5,
+                      n.y = 5,
+                      projection = "+proj=longlat",
+                      alpha = 0.35)
+    }
   
 }
 
