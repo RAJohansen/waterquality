0c6aad7db6ac0a3322e2bc421a0013e532ba169f
Richard Johansen <Richard.A.Johansen@erdc.dren.mil>
2022-02-07 10:58:29 -0600

    Updated Test that docs

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,5 @@ context("wq_algorithms")
 test_that("wq_algortihms are correct", {
   # data is intact
   expect_equal(unique(wq_algorithms$type), c("chlorophyll", "phycocyanin", "turbidity"))
-  expect_equal(dim(wq_algorithms), c(177, 5))
+  expect_equal(dim(wq_algorithms), c(161, 5))
 })
